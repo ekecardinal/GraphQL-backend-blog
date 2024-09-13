50c5e2d606517c5875d37600c2c5718cc63e3092
ekecardinal <ekecardinal@gmail.com>
2024-09-13 12:43:54 +0100

    testing 2

--- api/serverless.ts
+++ api/serverless.ts
@@ -1,51 +1,21 @@
 import { NestFactory } from '@nestjs/core';
-import {
-  BadRequestException,
-  INestApplication,
-  ValidationPipe,
-} from '@nestjs/common';
+import { AppModule } from '../src/app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { Request, Response } from 'express';
 import * as graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress.js';
 import * as cookieParser from 'cookie-parser';
-import { Request, Response } from 'express';
-import { GraphQLErrorFilter } from 'src/filters/custom-exception.filter';
-import { AppModule } from 'src/app.module';
 
-let server: INestApplication;
+let server: NestExpressApplication;
 
 const bootstrap = async () => {
   if (!server) {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
     app.enableCors({
       origin: '*', // Adjust according to your needs
       credentials: true,
-      allowedHeaders: [
-        'Accept',
-        'Authorization',
-        'Content-Type',
-        'X-Requested-With',
-        'apollo-require-preflight',
-      ],
-      methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
     });
     app.use(graphqlUploadExpress({ maxFileSize: 10000000000, maxFiles: 10 }));
     app.use(cookieParser());
-    app.useGlobalPipes(
-      new ValidationPipe({
-        whitelist: true,
-        transform: true,
-        exceptionFactory: (errors) => {
-          const formattedErrors = errors.reduce((accumulator, error) => {
-            accumulator[error.property] = Object.values(error.constraints).join(
-              ', ',
-            );
-            return accumulator;
-          }, {});
-          console.log('formattedErrors123', formattedErrors);
-          throw new BadRequestException(formattedErrors);
-        },
-      }),
-    );
-    app.useGlobalFilters(new GraphQLErrorFilter());
     server = app;
     await app.init();
   }
